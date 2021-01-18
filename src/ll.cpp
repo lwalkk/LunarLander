bbe87c6ff4b36002667c9511e3d4adad92ea0d41
devynzlewis <73604162+devynzlewis@users.noreply.github.com>
2021-01-18 16:56:24 -0500

    Update ll.cpp

--- src/ll.cpp
+++ src/ll.cpp
@@ -118,9 +118,9 @@ int main( int argc, char **argv )
   glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
   glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
 #else
-  glfwWindowHint( GLFW_CLIENT_API, GLFW_OPENGL_ES_API );
-  glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-  glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 0 );
+  //glfwWindowHint( GLFW_CLIENT_API, GLFW_OPENGL_ES_API );
+  //glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
+  //glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 0 );
 #endif
 
   window = glfwCreateWindow( screenAspect * screenWidth, screenWidth, "Lunar Lander", NULL, NULL);
