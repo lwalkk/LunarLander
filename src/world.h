77327bb7d61e3549f482087101768005ef5fa5f3
Liam Walker <37026953+lwalkk@users.noreply.github.com>
2021-01-18 16:36:55 -0500

    professors code

---
+++ src/world.h
@@ -0,0 +1,56 @@
+// world.h
+
+
+#ifndef WORLD_H
+#define WORLD_H
+
+
+#include "headers.h"
+#include "landscape.h"
+#include "lander.h"
+#include "ll.h"
+
+
+#define BOTTOM_SPACE 0.1f // amount of blank space below terrain (in viewing coordinates) 
+
+
+class World {
+
+  Landscape *landscape;
+  Lander    *lander;
+  bool       zoomView; // show zoomed view when lander is close to landscape
+  GLFWwindow *window;
+
+  char *showMsg;
+  int  score;
+
+ public:
+
+  World( GLFWwindow *w ) {
+    landscape = new Landscape();
+    lander    = new Lander( maxX(), maxY() ); // provide world size to help position lander
+    zoomView  = false;
+    window    = w;
+    showMsg   = NULL;
+    score     = 0;
+  }
+
+  void draw();
+
+  void updateState( float elapsedTime );
+
+  void resetLander() {
+    lander->reset();
+  }
+
+  // World extremes (in world coordinates)
+
+  float minX() { return 0; }
+  float maxX() { return landscape->maxX(); }
+
+  float minY() { return 0; }
+  float maxY() { return (landscape->maxX() - landscape->minX()) / screenAspect * (2 - BOTTOM_SPACE) / 2; }
+};
+
+
+#endif
